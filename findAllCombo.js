0736af8447b6709e7c802a424ce8d3d23f2f09ce
Shirley Wu <shirleyswwu@gmail.com>
2020-11-03 13:44:48 -0800

    dif length all combinations completed awesome

--- findAllCombo.js
+++ findAllCombo.js
@@ -13,18 +13,18 @@ output: [
   // call findAllAnagram on each character
 let anagrams = [];
 function callAnagram(word) {
-  const anagramSize = word.length - 1;
-  return allAnagrams(word);
+  const wordSize = word.length;
+  return allAnagrams(wordSize, word);
 }
 
-var allAnagrams = function(word, anagram = ''， anagramSize = word.length) {
+var allAnagrams = function(wordSize, word, anagram = '') {
   // if word is empty than push it into anagram result
   if (!word) {
     anagrams.push(anagram);
     return;
   }
 
-  if (anagram.length >= 1 && anagram.length <= anagramSize) {
+  if (anagram.length >= 1 && anagram.length < wordSize) {
     anagrams.push(anagram);
   }
 
@@ -32,13 +32,13 @@ var allAnagrams = function(word, anagram = ''， anagramSize = word.length) {
     // select letter our anagram begins with (every letter if of input word will have a chance to be first letter)
     anagram += word[i];
     // call fn again without the letter we just added into anagram
-    allAnagrams(word.slice(0,i) + word.slice(i + 1), anagram);
+    allAnagrams(wordSize, word.slice(0,i) + word.slice(i + 1), anagram);
     // slice out last letter
     anagram = anagram.slice(0, anagram.length - 1);
   }
 };
 
-allAnagrams('abc'); //this is our word to work with
+callAnagram('abcd'); //this is our word to work with
 console.log(anagrams);
 
 
