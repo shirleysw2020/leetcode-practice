07c5692e508eef44dd00cf5740461f0a9bcce55d
Shirley Wu <shirleyswwu@gmail.com>
2020-11-11 00:01:17 -0800

    complete 98 corrections

--- 98.validateBinarySearchTree.js
+++ 98.validateBinarySearchTree.js
@@ -14,14 +14,14 @@
  */
 var isValidBST = function(root) {
   // initialize min val as -infinity and max val as pos infinity
-  dfs(root, -Infinity, Infinity);
+  return dfs(root, -Infinity, Infinity);
 
   function dfs(root, minVal, maxVal) {
     if (!root) return true;
     //root's val shouldn't be greater than it's right side value maxVal , these conditions outrules a BST
-    if (root.val >= maxVal  || rot.val <= minVal) return false;
+    if (root.val >= maxVal  || root.val <= minVal) return false;
     // keep searching on left and right
     // for left, min val is minVal wich is the current's root's minval, but max will be the root itself, because the root's left should be less thant he root itself
-    return dfs(root.left, minVal, root.val) && dfs(root.right, maxVal, root.val);
+    return dfs(root.left, minVal, root.val) && dfs(root.right, root.val, maxVal);
   }
 };
