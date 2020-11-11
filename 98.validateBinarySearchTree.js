35655323df2960f7b1887d1f5959481dd12adc58
Shirley Wu <shirleyswwu@gmail.com>
2020-11-10 23:59:39 -0800

    complete 98 validate bst

---
+++ 98.validateBinarySearchTree.js
@@ -0,0 +1,27 @@
+/*
+98. Validate Binary Search Tree.
+*//**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+  // initialize min val as -infinity and max val as pos infinity
+  dfs(root, -Infinity, Infinity);
+
+  function dfs(root, minVal, maxVal) {
+    if (!root) return true;
+    //root's val shouldn't be greater than it's right side value maxVal , these conditions outrules a BST
+    if (root.val >= maxVal  || rot.val <= minVal) return false;
+    // keep searching on left and right
+    // for left, min val is minVal wich is the current's root's minval, but max will be the root itself, because the root's left should be less thant he root itself
+    return dfs(root.left, minVal, root.val) && dfs(root.right, maxVal, root.val);
+  }
+};
