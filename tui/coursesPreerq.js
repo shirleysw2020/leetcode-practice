3d10718160de2e227480d662c6b09fc9ca55af6c
Shirley Wu <shirleyswwu@gmail.com>
2020-11-29 21:04:09 -0800

    practice prob

--- tui/coursesPreerq.js
+++ tui/coursesPreerq.js
@@ -1,12 +1,4 @@
-//oa
-/*
-DataStructures Algorithms 4
-FoundationsOfCS OperatingSystems 6
-ComputerNetworks ComputerArchitecture 2
-Algorithms FoundationsOfCS 5
-ComputerArchitecture DataStructures 3
-SoftwareDesign ComputerNetworks 1
-*/
+//
 /*
 input: [][] : [
   [SoftwareDesign ComputerNetworks],
@@ -41,20 +33,13 @@ const input = [
 let courseSchedule = function(courses) {
   let m = new Map(); // tracks course and prereq relationship
   let indegree = new Map(); //tracks num of precourse needed
-  let mid;
-  if (courses.length % 2 == 0) { //we want the halfway course, diff for even and odd length
-    mid = Math.floor((courses.length+1)/2);
-  } else {
-    mid = Math.floor((courses.length)/2);
-  }
-  for (let pair of courses) { //O(n) or O(V + E)where v is vertices and e is edges
+  let mid = Math.floor((courses.length)/2);
+  if (courses.length % 2 == 0) mid -= 1;
+
+  for (let pair of courses) { //O(n) or O(V + E)v is vertices, e is edges
     m.set(pair[0], pair[1]);
-    if (!indegree.has(pair[0])) {
-      indegree.set(pair[0], 0);
-    }
-    if (!indegree.has(pair[1])) {
-      indegree.set(pair[1], 0);
-    }
+    if (!indegree.has(pair[0])) indegree.set(pair[0], 0);
+    if (!indegree.has(pair[1])) indegree.set(pair[1], 0);
     indegree.set(pair[1], indegree.get(pair[1]) + 1);
   }
   let queue = [];
@@ -74,8 +59,6 @@ let courseSchedule = function(courses) {
 
 console.log(courseSchedule(input));
 
-//space: O(n)
-
 // const input = [
 //   ['SoftwareDesign', 'ComputerNetworks'],
 //   ['ComputerNetworks', 'test2'],
@@ -87,14 +70,3 @@ console.log(courseSchedule(input));
 //   ['Algorithms', 'FoundationsOfCS'],
 //   ['FoundationsOfCS', 'OperatingSystems']
 // ];
-
-    // if (!indegree.has(pair[0])) {
-    //   indegree.set(pair[0], 0); //no prereq is 0
-    // } else {
-    //   indegree.set(pair[0], indegree.get(pair[0])+1);
-    // }
-    // if (!indegree.has(pair[1])) {
-    //   indegree.set(pair[1], 1); //need prereq is 2
-    // } else {
-    //   indegree.set(pair[1], indegree.get(pair[1])+1);
-    // }
